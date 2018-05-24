d52e39ceb07c44cc564460d4349220321bce9709
Kyriakos Kyrkis <kyriakos.kyrkis@faktorzehn.de>
2018-05-24 03:32:58 +0200

    Problem 4 solved.

---
+++ src/main/java/problem0004_largestPalindromeProduct/LargestPalindromeProduct.java
@@ -0,0 +1,34 @@
+package problem0004_largestPalindromeProduct;
+
+import java.util.Scanner;
+
+public class LargestPalindromeProduct {
+    private final static int MIN_N = 101101;
+
+    public static boolean isPalindrome(int n) {
+        String s = Integer.toString(n);
+        return new StringBuilder(s).reverse().toString().equals(s);
+    }
+
+    public static int find(int n) {
+        for (int i = n - 1; i >= MIN_N; i--) {
+            for (int j = 121; j < 1000; j++) {
+                if (i % 11 == 0) {
+                    if (isPalindrome(i) && j * j < i && i % j == 0 && i / j < 1000) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            System.out.println(find(n));
+        }
+    }
+}
